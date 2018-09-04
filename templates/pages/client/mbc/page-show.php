268f2113069944c0ca4a664a44c6c63ce368ee53
Rodrigo Tejero <rodrigo.tejero@thewebchi.mp>
2018-09-04 16:53:40 -0500

    Added button and style changes

--- templates/pages/client/mbc/page-show.php
+++ templates/pages/client/mbc/page-show.php
@@ -226,7 +226,7 @@
 			</div>
 			<div class="unit-meeting-info boxfix-vert">
 				<div class="margins">
-					<p class="text-center"><a href="<%= block.metas.meeting_link %>" class="button button-primary"><i class="fa fa-fw fa-user-circle-o"></i> Join the meeting</a></p>
+					<p class="text-center"><a href="<%= block.metas.meeting_link %>" target="_blank" class="button button-primary"><i class="fa fa-fw fa-user-circle-o"></i> Join the meeting</a></p>
 				</div>
 			</div>
 		</script>
